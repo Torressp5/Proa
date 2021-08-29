2995b5ec17beb292fb39a0925216ac84fa826bbe
Gustavo Torres <89090794+Torressp5@users.noreply.github.com>
2021-08-29 13:31:20 -0300

    Proa

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+function teste() {
+    document.getElementById('paragrafo').innerHTML = Date();
+}
+
+function Mudar() {
+    document.getElementById('paragrafo').style.fontSize = "20px";
+    document.getElementById('paragrafo').style.backgroundColor = "blue";
+}
